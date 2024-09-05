81c1665850526b47b3b8ed88f525cdb080d275eb
Cameron Smith <71192028+smith-cameron@users.noreply.github.com>
2024-09-04 17:02:15 -0700

    update

---
+++ W2D2/jsFunPt5.js
@@ -0,0 +1,33 @@
+// _ W2D2 More Msc JS Stuff
+
+// _ Switch
+// . switch (expression): 
+    // * This is the value that you want to evaluate
+// . case value: 
+    // * Each case represents a potential match for the expression. 
+    // * If the expression matches a `case` value, the corresponding block of code is executed.
+// . break;
+    // * exits the `switch` statement once a matching case is found
+// . default:
+    // * optional case that executes if none of the specified cases match the expression
+    // * equivalent of an else
+
+// _ Try - Catch - Finally
+// .    Executed first
+// ,      try { 
+// ,          tryStatements
+// ,      } 
+// .    If the try throws an exception
+// .    the code in the catch block will be executed.
+// ,      catch (exceptionVar) {
+// ,          catchStatements
+// ,      } 
+// .    Will always be executed
+// ,     finally {
+// ,         finallyStatements
+// ,     }
+
+// _ Positional/Default Arguments
+// * Positional arguments are the basic type of arguments passed to a function
+// * Default arguments are parameters that have predefined values 
+    // * if no value (or `undefined`) is passed when the function is called
